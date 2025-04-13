1d46f7264a7fb1ec93e25f233b0b8e24fd17b6ad
Jacob <jacc20223@gmail.com>
2025-04-13 14:45:25 -0500

    Crear primer pokemon

--- Assets/_Game/Scripts/ACDatos.cs
+++ Assets/_Game/Scripts/ACDatos.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class ACDatos
+public class ACDatos : MonoBehaviour
 {
     public int id;
     public string nombre;
