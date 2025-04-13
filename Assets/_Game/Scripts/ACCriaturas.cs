6561595a3a92f2e3e12243cf8129f9a181bf50bf
Jacob <jacc20223@gmail.com>
2025-04-13 13:23:52 -0500

    Edicion Scripts ACCriaturas y ACDexManager

--- Assets/_Game/Scripts/ACCriaturas.cs
+++ Assets/_Game/Scripts/ACCriaturas.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ACCriaturas : MonoBehaviour
+public class ACCriaturas
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<ACDatos> acCriaturas = new List<ACDatos>();
 }
--- Assets/_Game/Scripts/ACDexManager.cs
+++ Assets/_Game/Scripts/ACDexManager.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class ACDexManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private string _filePath;
+    public ACCriaturas _acCriaturas;
+
     void Start()
     {
-        
+        _filePath = Application.persistentDataPath + "/Dex.json";//Determina direccion en donde se guarda el json que tiene los datos guardados
+        CargarDex();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CargarDex()
     {
-        
+        if(File.Exists(_filePath))
+        {
+            string json = File.ReadAllText(_filePath);//si existe el archivo se lee la informacion
+            _acCriaturas = JsonUtility.FromJson<ACCriaturas>(json);//Recoger info json
+        }
+        else
+        {
+            _acCriaturas = new ACCriaturas();
+            GuardarDex();
+        }
+    }
+
+     public void GuardarDex()
+    {
+        string json = JsonUtility.ToJson(_acCriaturas, true);//Convertir informacion a json
+        File.WriteAllText(_filePath, json);//Funcion que guarda el json
     }
 }
