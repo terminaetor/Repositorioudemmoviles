0f79d7d9f7cd65eefe22524c6da4d9250de43168
Jacob <jacc20223@gmail.com>
2025-04-16 14:59:35 -0500

    Descarte scripts

--- Assets/_Game/Scripts/ACCriatura.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ACCriatura : MonoBehaviour
-{
-    public ACDatos datos;//Script que permite asignar desde el inspector los datos de la criatura, va atachado al gameobject de la criatura
-}
