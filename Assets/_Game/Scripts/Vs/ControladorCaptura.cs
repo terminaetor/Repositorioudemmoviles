b36c449310f42d62b7cc4549186d980e076a2287
Jacob <jacc20223@gmail.com>
2025-04-24 01:07:49 -0500

    instanciar criatura

--- Assets/_Game/Scripts/Vs/ControladorCaptura.cs
+++ Assets/_Game/Scripts/Vs/ControladorCaptura.cs
@@ -6,6 +6,26 @@ public class ControladorCaptura : MonoBehaviour
 {
     public Pokedex pokedex; // Asigna el ScriptableObject desde el inspector
     public Transform puntoSpawn;
+    public int id;
     private Criatura criaturaActual;
     private GameObject criaturaInstanciada;
+
+    private void Start()
+    {
+        PrepararCaptura(id);
+    }
+
+    public void PrepararCaptura(int idCriatura)
+    {
+        // Carga la criatura por ID desde la pokedex
+        criaturaActual = pokedex.GetCriaturaPorID(idCriatura);
+        if (criaturaActual == null)
+        {
+            Debug.LogError("No se encontró criatura con ese ID");
+            return;
+        }
+
+        // Instancia el prefab en la escena
+        criaturaInstanciada = Instantiate(criaturaActual.prefab, puntoSpawn.position, Quaternion.identity);
+    }
 }
